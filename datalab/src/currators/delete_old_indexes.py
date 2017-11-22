15b377674cea1385452ffa645f7df1b9adb9fb2c
user <user@ma015325.ads.eso.org>
2017-11-22 19:20:53 -0300

    Correction currators /delete_old_indexes

--- datalab/src/currators/delete_old_indexes.py
+++ datalab/src/currators/delete_old_indexes.py
@@ -1,55 +1,54 @@
-import elasticsearch
+
+
 import curator
+import elasticsearch
 
+ONE_WEEK_INDEXES_PREFIX = ['.monitoring-es-6-', '.watcher-history-6' ]
+ONE_MONTH_INDEXES_PREFIX = []
+ONE_YEAR_INDEXES_PREFIX = []
 
-if __name__ == '__main__':
+client = elasticsearch.Elasticsearch("134.171.189.10:9200")
 
-    ONE_WEEK_INDEXES_PREFIX = ['.monitoring-es-6-', '.watcher-history-6' ]
-    ONE_MONTH_INDEXES_PREFIX = []
-    ONE_YEAR_INDEXES_PREFIX = []
+for my_prefix in ONE_WEEK_INDEXES_PREFIX:
 
-    client = elasticsearch.Elasticsearch("134.171.189.10:9200")
+    ilo = curator.IndexList(client)
 
-    for my_prefix in ONE_WEEK_INDEXES_PREFIX:
+    ilo.filter_by_regex(kind='prefix', value=my_prefix)
 
-        ilo = curator.IndexList(client)
+    ilo.filter_by_age(source='creation_date', direction='older', unit='days', unit_count=7)
 
-        ilo.filter_by_regex(kind='prefix', value=my_prefix)
-
-        ilo.filter_by_age(source='creation_date', direction='older', unit='days', unit_count=7)
-
-        try :
-            curator.DeleteIndices(ilo=ilo, master_timeout=60*60*12).do_action()
-        except curator.NoIndices:
-            pass
+    try :
+        curator.DeleteIndices(ilo=ilo, master_timeout=60*60*12).do_action()
+    except curator.NoIndices:
+        pass
 
 
 
-    for my_prefix in ONE_MONTH_INDEXES_PREFIX:
+for my_prefix in ONE_MONTH_INDEXES_PREFIX:
 
-        ilo = curator.IndexList(client)
+    ilo = curator.IndexList(client)
 
-        ilo.filter_by_regex(kind='prefix', value=my_prefix)
+    ilo.filter_by_regex(kind='prefix', value=my_prefix)
 
-        ilo.filter_by_age(source='creation_date', direction='older', unit='months', unit_count=1)
+    ilo.filter_by_age(source='creation_date', direction='older', unit='months', unit_count=1)
 
-        try:
-            curator.DeleteIndices(ilo=ilo, master_timeout=60 * 60 * 12).do_action()
-        except curator.NoIndices:
-            pass
+    try:
+        curator.DeleteIndices(ilo=ilo, master_timeout=60 * 60 * 12).do_action()
+    except curator.NoIndices:
+        pass
 
-    for my_prefix in ONE_YEAR_INDEXES_PREFIX:
+for my_prefix in ONE_YEAR_INDEXES_PREFIX:
 
-        ilo = curator.IndexList(client)
+    ilo = curator.IndexList(client)
 
-        ilo.filter_by_regex(kind='prefix', value=my_prefix)
+    ilo.filter_by_regex(kind='prefix', value=my_prefix)
 
-        ilo.filter_by_age(source='creation_date', direction='older', unit='years', unit_count=1)
+    ilo.filter_by_age(source='creation_date', direction='older', unit='years', unit_count=1)
 
-        try:
-            curator.DeleteIndices(ilo=ilo, master_timeout=60 * 60 * 12).do_action()
-        except curator.NoIndices:
-            pass
+    try:
+        curator.DeleteIndices(ilo=ilo, master_timeout=60 * 60 * 12).do_action()
+    except curator.NoIndices:
+        pass
 
 
 
--- setup.py
+++ setup.py
@@ -9,13 +9,14 @@ here = path.abspath(path.dirname(__file__))
 #with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 #    long_description = f.read()
 
+#twine upload dist/*
 setup(
     name='jose-datalab',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.3',
+    version='0.0.4',
 
     description='Datalab project python-api',
 #    long_description=long_description,
